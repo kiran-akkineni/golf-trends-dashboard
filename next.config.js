3ce4e1a424a0dcf2ca9389da712ddc8ddd176365
Kiran Akkineni <kiran.home@gmail.com>
2026-03-17 21:40:33 -0700

    Fix: add undici to serverComponentsExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['https-proxy-agent'],
+    serverComponentsExternalPackages: ['undici', 'https-proxy-agent'],
   },
 };
 
